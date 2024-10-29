0f90b09a1b085504558d0f02c4854223a057e1a9
Nipun Wijayakoon <nipun.wijayakoon@globalwavenet.com>
2024-10-29 22:39:53 +0530

    responsive_design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import {MatButtonModule} from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { DevstackComponent } from './components/devstack/devstack.component';
+import { ResponsiveComponent } from './components/responsive/responsive.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatFormFieldModule, MatInputModule,DevstackComponent],
+  imports: [RouterOutlet, MatButtonModule, MatFormFieldModule, MatInputModule,DevstackComponent, ResponsiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/responsive/responsive.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsive',
+  standalone: true,
+  imports: [],
+  templateUrl: './responsive.component.html',
+  styleUrl: './responsive.component.scss'
+})
+export class ResponsiveComponent {
+
+}
